85e0b27e5eb49bcf2c34b30cdc884c3a90785682
satonaka214 <spt85wx9@gmail.com>
2019-05-16 20:46:34 +0900

    極力 Spring ライブラリに依存せずトグル制御できるようにした

--- src/main/java/com/satonaka/togglztest/myfeatures/MyFeatures.java
+++ src/main/java/com/satonaka/togglztest/myfeatures/MyFeatures.java
@@ -1,10 +1,12 @@
-package com.satonaka.togglztest.config;
+package com.satonaka.togglztest.myfeatures;
 
 import org.togglz.core.Feature;
+import org.togglz.core.annotation.EnabledByDefault;
 import org.togglz.core.annotation.Label;
 import org.togglz.core.context.FeatureContext;
 
 public enum MyFeatures implements Feature {
+    @EnabledByDefault
     @Label("First Feature")
     FEATURE_ONE,
 
