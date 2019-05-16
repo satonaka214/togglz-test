7520fa78e127478d7198bf4f23926592d86d51fe
satonaka214 <spt85wx9@gmail.com>
2019-05-16 18:14:03 +0900

    first commit

---
+++ src/main/java/com/satonaka/togglztest/TogglzTestApplication.java
@@ -0,0 +1,13 @@
+package com.satonaka.togglztest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogglzTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogglzTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satonaka/togglztest/TogglzTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.satonaka.togglztest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TogglzTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
