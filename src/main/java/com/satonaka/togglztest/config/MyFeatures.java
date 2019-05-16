7c30da622986fc8d55928cd6b5d087d184afc53f
satonaka214 <spt85wx9@gmail.com>
2019-05-16 19:24:23 +0900

    MyFeatures クラスを追加した

---
+++ src/main/java/com/satonaka/togglztest/config/MyFeatures.java
@@ -0,0 +1,17 @@
+package com.satonaka.togglztest.config;
+
+import org.togglz.core.Feature;
+import org.togglz.core.annotation.Label;
+import org.togglz.core.context.FeatureContext;
+
+public enum MyFeatures implements Feature {
+    @Label("First Feature")
+    FEATURE_ONE,
+
+    @Label("Second Feature")
+    FEATURE_TWO;
+
+    public boolean isActive() {
+        return FeatureContext.getFeatureManager().isActive(this);
+    }
+}
