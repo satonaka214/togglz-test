83ac35aabfb101cf64bdbb97e83aa932694433a0
rairai214 <spt85wx9@gmail.com>
2019-05-19 10:32:14 +0900

    Admin console を使用できるようにした

--- src/main/java/com/satonaka/togglztest/myfeatures/MyFeaturesManagerBean.java
+++ src/main/java/com/satonaka/togglztest/myfeatures/MyFeaturesManagerBean.java
@@ -1,8 +1,10 @@
 package com.satonaka.togglztest.myfeatures;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.togglz.core.context.StaticFeatureManagerProvider;
 import org.togglz.core.manager.FeatureManager;
 import org.togglz.core.manager.FeatureManagerBuilder;
 
@@ -13,8 +15,12 @@ public class MyFeaturesManagerBean {
 
     @Bean
     FeatureManager featureManager() {
-        return new FeatureManagerBuilder()
+        val manager = new FeatureManagerBuilder()
                 .togglzConfig(configuration)
                 .build();
+        StaticFeatureManagerProvider.setFeatureManager(manager);
+
+        return manager;
     }
+
 }
